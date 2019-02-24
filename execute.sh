73f0bf9cc3dc34ef08c54bf53ff30caf2a68afe9
yangzonglin <806765278@qq.com>
2019-02-24 21:25:30 +0800

    1. Add `dataset` argument for `execute.sh` script. 2. ELMo model checkpoint directory now contains date information.

--- execute.sh
+++ execute.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-    echo 'Usage: operation'
+set -x
+set -e
+
+if [[ $# -ne 2 ]]; then
+    echo 'Usage: operation dataset'
     exit -1
 fi
 operation=$1
+dataset=$2
 
 case "$operation" in
 'make_vocab')
-    python bin/make_vocab.py --train-data=data/toy/toy-train.bpe \
-    --vocab-fname bin/processed/toy.vocab
+    python bin/make_vocab.py --train-data=data/${dataset}/${dataset}-train.bpe \
+    --vocab-fname bin/processed/${dataset}.vocab
     ;;
 'prepare_data')
-    python bin/prepare_data.py --train-data=data/toy/toy-train.bpe \
-    --split-train-dir=bin/processed/toy-train/ \
-    --split-heldout-dir=bin/processed/toy-test/ \
+    python bin/prepare_data.py --train-data=data/${dataset}/${dataset}-train.bpe \
+    --split-train-dir=bin/processed/${dataset}-train/ \
+    --split-heldout-dir=bin/processed/${dataset}-test/ \
     --split-train-num=0 --split-heldout-num=4 \
     --test-samples-num=40
     ;;
 'train_elmo')
-    python bin/train_elmo.py --train_prefix=bin/processed/toy-train/* \
-    --vocab_file=bin/processed/toy.vocab \
-    --save_dir=checkpoints/bilm-toy/
+    # like 2019-02-24_21:08:22
+    CHECKPOINT_DIR=checkpoints/bilm-${dataset}_`date +%F_%T`
+    mkdir -p ${CHECKPOINT_DIR}
+    python bin/train_elmo.py --train_prefix=bin/processed/${dataset}-train/* \
+    --vocab_file=bin/processed/${dataset}.vocab \
+    --save_dir=${CHECKPOINT_DIR}/
     ;;
 *)
     echo 'error'
